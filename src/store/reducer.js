e101959f254039aee63d663a6b3450330403f991
Yongxin Wang <umay.yx.wang@gmail.com>
2020-07-06 23:26:04 +1000

    added immutable

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,8 @@
-const defaultState = {};
+import {combineReducers} from 'redux-immutable';
+import {reducer as headerReducer} from '../common/header/store';
+import {reducer as homeReducer} from '../pages/home/store';
 
-export default (state=defaultState, action) => {
-	return state;
-}
+export default combineReducers({
+	header: headerReducer,
+	home: homeReducer
+})
